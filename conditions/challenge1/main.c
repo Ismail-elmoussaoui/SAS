42a2409e077787bb7a5992a6ec1728e25a843a48
ismail <elmoussaouiismail86@gmail.com>
2024-09-27 11:52:01 +0100

    Add files via upload

---
+++ conditions/challenge1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+   printf("entrer le nomrbre reel\n");
+   scanf("%d",&n);
+   if (n%2==0){
+    printf("le nombre est pair");
+
+   else printf("le nombre est impaire");
+
+    return 0;
+}
